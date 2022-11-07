fe3aaaffa412e89939c5137babf883c48424ad02
Caraidas <76404735+Caraidas@users.noreply.github.com>
2022-11-07 10:40:44 +0100

    mis à propre du code

--- wiktionnary/src/main/java/wiktionnary/wiktionnary/App.java
+++ wiktionnary/src/main/java/wiktionnary/wiktionnary/App.java
@@ -17,10 +17,10 @@ public class App
 			BufferedReader br=new BufferedReader(new FileReader(file));  
     		System.out.println("file content: ");  
     		String r;  
-    		int compt = 0;
-    		//int compt1 = 0; 
-    		int compt2 = 0;
-    		int compt3 = 0;
+    		boolean estMot = false;
+    		//int compt1 = 0; // a décommenté pour avoir les 500 1ers lignes
+    		boolean titre = false;
+    		boolean estFrancais = false;
     		String mot = "";
     		while((r = br.readLine()) != null)  
     		{  
@@ -30,28 +30,31 @@ public class App
     			if (r.contains("<title>")) {
     				mot = r.replace("    <title>", "");
     				mot = mot.replace("</title>", "");
-    				compt2 = 1;
+    				titre = true;
     			} // 
     			
     			//la balise <ns> suivis de 0 signifie que ce contient title est un mot
-    			if(r.contains("<ns>0") && compt2 == 1) {
-    				compt = 1;
+    			if(r.contains("<ns>0") && titre) {
+    				estMot = true;
     			}
     			
-    			if(r.contains("== {{langue|fr}} ==") && compt == 1) {
-    				compt3 = 1;
+    			//vérifie que le mot est français
+    			if(r.contains("== {{langue|fr}} ==") && estMot) {
+    				estFrancais = true;
     				System.out.println(mot);
     			}
     			
     			//permet d'avoir la 1er définition du mot rechercher
-    			if(compt3 == 1 && r.startsWith("#") && !(r.startsWith("#*")) && !(r.startsWith("##*")) && !(r.startsWith("###*"))) {
+    			if(estFrancais && r.startsWith("#") && !(r.startsWith("#*")) && !(r.startsWith("##*")) && !(r.startsWith("###*"))) {
     				System.out.println(r);
     				//break;
     			}
-    			if(r.startsWith(" ") && compt3 == 1) {
-    				compt = 0;
-    				compt2 = 0;
-    				compt3 = 0;
+    			
+    			//une fois la lecture des définitions du mot terminée reset les varaibles
+    			if(r.startsWith(" ") && estFrancais) {
+    				estMot = false;
+    				titre = false;
+    				estFrancais = false;
     			}
     				
     		// décomentez pour avoir le mot accueil et lire
