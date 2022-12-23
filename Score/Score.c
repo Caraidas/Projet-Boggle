2a8aa4e2289f6fe56729b9edc59b928ea0ffef9a
Caraidas <76404735+Caraidas@users.noreply.github.com>
2022-12-23 04:19:17 +0100

    Score sans bonus

--- Score/Score.c
+++ Score/Score.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
-int main(int argc, char* argv[]){
 
+int main(int argc, char* argv[]){
+    int total = 0;
+
+    if(argc == 0){
+        printf("0\n");
+        return 0;
+    }
+    for (int i = 1;i<argc;i++){
+
+        total ++;
+    }
+
+    printf("%d\n",total);
     return 0;
 }
