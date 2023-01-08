554dbcaa79fa5ae6d9dc9a6293c7bb3e3491b705
Caraidas <76404735+Caraidas@users.noreply.github.com>
2023-01-08 03:44:35 +0100

    Score avec plus de points

--- Score/Score.c
+++ Score/Score.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 
 int main(int argc, char* argv[]){
@@ -9,7 +10,22 @@ int main(int argc, char* argv[]){
         return 0;
     }
     for (int i = 1;i<argc;i++){
-
+        if (strlen(argv[i])>5 ){
+            total += 2;
+        }
+        if (strlen(argv[i])>8){
+            total += 2;
+        }
+        if (strlen(argv[i])>12){
+            total += 5;
+        }
+        for (int j = 0; argv[i][j] != '\0'; j++) {
+            if (argv[i][j] == 'X' || argv[i][j] == 'W' || argv[i][j] == 'Z' || argv[i][j] == 'K' || argv[i][j] == 'Y' ){
+                total += 5;
+            }
+            if (argv[i][j] == 'H' || argv[i][j] == 'G' || argv[i][j] == 'Q' || argv[i][j] == 'V' || argv[i][j] == 'B' || argv[i][j] == 'F')
+            total += 3;
+        }
         total ++;
     }
 
