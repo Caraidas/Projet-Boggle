2ffb83b95194df8e682bf98e0d6efc8d368b4129
Meledit <99364115+Meledit@users.noreply.github.com>
2023-01-22 22:13:57 +0100

    fix incri

--- site/php/script-inscription.php
+++ site/php/script-inscription.php
@@ -10,9 +10,10 @@
 
     //Préparation de la requête
     $result = $cnx->count("SELECT pseudo FROM B_JOUEUR WHERE '$pseudo' = pseudo;");
-    if($result>0){
+    if ($result > 0) {
         header("Location: ../html/inscription.html");
+    } else {
+        $cnx->i("INSERT INTO B_JOUEUR (pseudo, mail, mdp, xp_actuel, photo_de_profil, compte_prive) VALUES('$pseudo', '$mail', '$password', 0, null, 0);");
+        header("Location: ../html/connexion.html");
     }
-    $cnx->i("INSERT INTO B_JOUEUR (pseudo, mail, mdp, xp_actuel, photo_de_profil, compte_prive) VALUES('$pseudo', '$mail', '$password', 0, null, 0);");
-    header("Location: ../html/connexion.html");
 
