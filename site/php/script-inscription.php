2a2f86e009d5d84e868194909570b1086ac24046
Caraidas <76404735+Caraidas@users.noreply.github.com>
2023-01-22 22:09:32 +0100

    check si le pseudo existe

--- site/php/script-inscription.php
+++ site/php/script-inscription.php
@@ -9,6 +9,10 @@
     echo $password;
 
     //Préparation de la requête
+    $result = $cnx->count("SELECT pseudo FROM B_JOUEUR WHERE '$pseudo' = pseudo;");
+    if($result>0){
+        header("Location: ../html/inscription.html");
+    }
     $cnx->i("INSERT INTO B_JOUEUR (pseudo, mail, mdp, xp_actuel, photo_de_profil, compte_prive) VALUES('$pseudo', '$mail', '$password', 0, null, 0);");
     header("Location: ../html/connexion.html");
 
