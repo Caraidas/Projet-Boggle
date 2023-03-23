f21f3e02642e2f33fd25d0362cf46094af3fdd82
Meledit <99364115+Meledit@users.noreply.github.com>
2023-03-23 10:22:00 +0100

    React crée

---
+++ froggle-website/src/App.js
@@ -0,0 +1,9 @@
+import './App.css';
+
+function App() {
+  return (
+    <h1>Happy coding Laura !</h1>
+  );
+}
+
+export default App;
--- moteur-de-jeu/test.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-
-int main(int argc, char* argv[]) {
-
-    printf("%s\n", argv[1]);
-    return 0;
-}
